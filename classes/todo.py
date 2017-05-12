830f6725a53d3fc649ee28975043f066b2f1cc9a
James Kinyua <jameskinyua590@gmail.com>
2017-05-12 13:29:27 +0300

    Add a method create_collection in class Todo

--- classes/todo.py
+++ classes/todo.py
@@ -8,7 +8,16 @@ from . import Collection, Item
 from .. import TodoDB
 
 class Todo:
+    """
+    Controller class in the application todo list.
+    """
     def __init__(self):
         self.db = TodoDB()
         
-    def create_collection():
+    def create_collection(self, name):
+        """
+        Create a new collection of items.
+        """
+        collection = Collection(name)
+        self.db.add_collection(collection)
+        
