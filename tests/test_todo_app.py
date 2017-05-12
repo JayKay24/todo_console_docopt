7d2c86ff9fb952fd8e0eea5b4dbb377308bf70b0
James Kinyua <jameskinyua590@gmail.com>
2017-05-12 19:44:16 +0300

    create test suite item

--- tests/test_todo_app.py
+++ tests/test_todo_app.py
@@ -7,14 +7,16 @@ Created on Fri May 12 19:16:03 2017
 import unittest
 
 from test_collection import CollectionTest
+from test_item import ItemTest
 
 def suite():
     """
     Return a composite test suite of test cases.
     """
     collection = unittest.makeSuite(CollectionTest)
+    item = unittest.makeSuite(ItemTest)
     
-    return unittest.TestSuite((collection,))
+    return unittest.TestSuite((collection, item))
     
 if __name__ == '__main__':
     unittest.main(defaultTest='suite')
