3e391983526bc9c4138dd6cd9e7f087570c2beaf
James Kinyua <jameskinyua590@gmail.com>
2017-05-12 11:53:33 +0300

    Add attribute id, coll_id to class Item

--- classes/item.py
+++ classes/item.py
@@ -5,5 +5,7 @@ Created on Fri May 12 11:33:33 2017
 @author: James Kinyua
 """
 class Item:
-    def __init__(self, name):
+    def __init__(self, name, id=None, coll_id=None):
+        self.id = id
         self.name = name
+        self.coll_id = coll_id
