ad963d027a04dd9899a69b128c9810264f9b1b39
James Kinyua <jameskinyua590@gmail.com>
2017-05-12 11:33:15 +0300

    Create a class Collection

---
+++ classes/collection.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 12 11:29:08 2017
+
+@author: James Kinyua
+"""
+class Collection:
+    """
+    Class used to make a collection object.
+    """
+    def __init__(self, name):
+        self.name = name
+        self.items = []
