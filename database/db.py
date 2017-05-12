ee1f4eb5f5e7cbcf80dc5f2603576eb144a75e6c
James Kinyua <jameskinyua590@gmail.com>
2017-05-12 12:05:41 +0300

    Define functions connect_db and close_db

--- database/db.py
+++ database/db.py
@@ -4,4 +4,28 @@ Created on Fri May 12 11:50:26 2017
 
 @author: James Kinyua
 """
+import sqlite3
 
+from contextlib import closing
+
+db_name = 'todo.sqlite'
+# Define a global connection object
+conn = None
+
+def connect_db():
+    """
+    Connect to the database.
+    """
+    global conn
+    if not conn:
+        # Connect to the database and return a connection object.
+        conn = sqlite3.connect(db_name)
+        # Allow the use of column names when accessing the result sets.
+        conn.row_factory = sqlite3.Row
+        
+def close_db():
+    """
+    Close the database connection.
+    """
+    if conn:
+        conn.close()
